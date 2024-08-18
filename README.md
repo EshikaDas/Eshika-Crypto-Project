03b49f9d4322604467fa284684fe5e71f56e6472
Eshika Das <eshikadas2019@gmail.com>
2024-08-18 18:53:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Eshika Crypto Project
---
+++ src/functions/saveItemToWatchlist.js
@@ -0,0 +1,17 @@
+export const saveItemToWatchlist = (e, id) => {
+  e.preventDefault();
+  let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+
+  if (watchlist) {
+    if (!watchlist.includes(id)) {
+      watchlist.push(id);
+      alert(`${id.substring(0, 1).toUpperCase() + id.substring(1)} - added to the watchlist`);
+    } else {
+      alert(`${id.substring(0, 1).toUpperCase() + id.substring(1)} - is already added to the watchlist!`);
+    }
+  } else {
+    watchlist = [id];
+    alert(`${id.substring(0, 1).toUpperCase() + id.substring(1)} - added to the watchlist`);
+  }
+  localStorage.setItem("watchlist", JSON.stringify(watchlist));
+};
